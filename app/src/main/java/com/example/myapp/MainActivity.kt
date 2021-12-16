d405fe69786fe7c96cd96941a7776338fc24e6f4
user <hojiakbar_1997@mail.ru>
2021-12-16 16:07:58 +0600

    Initial commit

---
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.myapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.core.graphics.toColor
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var temp = false
+        findViewById<Button>(R.id.btn1).setOnClickListener(View.OnClickListener {
+            if (temp){
+                R.id.btn1.toColor().green()
+            }else{
+                R.id.btn1.toColor().blue()
+            }
+        })
+    }
+
+
+
+
+
+
+}
